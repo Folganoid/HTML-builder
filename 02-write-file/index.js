8f2d576e83869e6f0c1f3292601febb75acfcc66
Andrii Holubkov <folganoid@yahoo.com>
2022-10-26 15:12:06 +0300

    feat: 2.Write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const {stdin} = process;
+
+let fileName = path.join(__dirname, path.sep,'text.txt');
+let fileStream = fs.createWriteStream(fileName, {encoding: 'utf8'});
+
+console.log('Enter your text:');
+stdin.on('data', data => {
+  if (data.toString().trim().toUpperCase() == 'EXIT') {
+    exit();
+  } else {
+    fileStream.write(data);
+  }
+});
+
+process.on('SIGINT', () => {
+  exit();
+});
+
+function exit() {
+  fileStream.end();
+  stdin.end('\nThanks and have a nice day !!!\n');
+  process.exit(0);
+}
+
