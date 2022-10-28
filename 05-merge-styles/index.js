3e1a8f088aa6830bd7f57a8bb4cc73ddcf2bddf1
Andrii Holubkov <folganoid@yahoo.com>
2022-10-28 14:34:55 +0300

    fix: 5.Merge styles read stream

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -12,7 +12,9 @@ readdir(dir).then((res) => {
     if (path.extname(file) != '.css') continue;
     fs.stat(path.join(dir,file), (err, stats) => {
       if (!stats.isDirectory()) {
-        fs.createReadStream(path.join(dir, file)).on('data', () => fileStream.write('\n/* ------'+ file +' ------ */\n')).pipe(fileStream);
+        fs.createReadStream(path.join(dir, file))
+          .on('data', () => fileStream.write('\n/* ------' + file + ' ------ */\n'))
+          .pipe(fileStream);
       }
     });
   }
